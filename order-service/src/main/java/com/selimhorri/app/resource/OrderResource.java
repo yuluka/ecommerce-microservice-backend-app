99e5a95fa4a12207beca393b7f68bb5b066a082d
Yuluka Gigante Muriel <yulukeitor@gmail.com>
2025-05-28 12:05:41 -0500

    feat: add health endpoint for order-service

--- order-service/src/main/java/com/selimhorri/app/resource/OrderResource.java
+++ order-service/src/main/java/com/selimhorri/app/resource/OrderResource.java
@@ -29,6 +29,11 @@ public class OrderResource {
 	
 	private final OrderService orderService;
 	
+	@GetMapping("/health")
+	public ResponseEntity<String> health() {
+		return ResponseEntity.ok("Order Service is up and running!");
+	}
+
 	@GetMapping
 	public ResponseEntity<DtoCollectionResponse<OrderDto>> findAll() {
 		log.info("*** OrderDto List, controller; fetch all orders *");
