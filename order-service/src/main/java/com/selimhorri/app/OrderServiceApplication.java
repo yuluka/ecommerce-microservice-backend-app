ec2fde4c4e0fe847ee4bd90c4ff46c3b726d6060
Yuluka Gigante Muriel <yulukeitor@gmail.com>
2025-05-30 01:43:39 -0500

    feat: modify order-service to connect to eureka

--- order-service/src/main/java/com/selimhorri/app/OrderServiceApplication.java
+++ order-service/src/main/java/com/selimhorri/app/OrderServiceApplication.java
@@ -9,27 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 public class OrderServiceApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
-	
-	
-	
+
 }
 
 @RestController
 class OrderController {
-	
+
 	@GetMapping
 	public String msg() {
 		return "Order controller responding!!";
 	}
-	
+
 }
-
-
-
-
-
-
