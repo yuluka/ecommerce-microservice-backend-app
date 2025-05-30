48d51d78a77c15668b120dc7ccdb911628425edc
Yuluka Gigante Muriel <yulukeitor@gmail.com>
2025-05-30 01:37:29 -0500

    feat: modify product-service to connect to eureka

--- cloud-config/src/main/java/com/selimhorri/app/CloudConfigApplication.java
+++ cloud-config/src/main/java/com/selimhorri/app/CloudConfigApplication.java
@@ -9,17 +9,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @EnableConfigServer
 public class CloudConfigApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigApplication.class, args);
 	}
-	
-	
-	
+
 }
-
-
-
-
-
-
